50720c3182478ae9bf31f76d163276109c7eca20
Océ <oceane.chabrerie.33@gmail.com>
2023-05-17 15:50:44 +0200

    fixtures added

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
