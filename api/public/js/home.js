35585ce38699ddcbe0190b4ca5b8626498561154
LouiseVen <louise.venuleth@laposte.net>
2023-05-23 16:06:32 +0200

    adds homepage

---
+++ api/public/js/home.js
@@ -0,0 +1,15 @@
+function openRegister() {
+    let div = document.createElement('div');
+    
+    div.classList.toggle('show');
+
+    document.getElementsByTagName('body')[0].appendChild(div);
+}
+
+function openLogin() {
+    let div = document.createElement('div');
+
+    div.classList.toggle('show');
+
+    document.getElementsByTagName('body')[0].appendChild(div);
+}
