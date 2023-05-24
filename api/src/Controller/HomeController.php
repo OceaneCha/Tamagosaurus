66670edcd4acf631c46c1dac5eff283dc0ec6aeb
LouiseVen <louise.venuleth@laposte.net>
2023-05-24 12:28:51 +0200

    Adds just a bit of JS

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/app', name: 'app_')]
 class HomeController extends AbstractController
 {
-    #[Route('/app', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/egg', name: 'egg')]
+    public function eggChoice(): Response
+    {
+        return $this->render('home/eggChoice.html.twig');
     }
 }
