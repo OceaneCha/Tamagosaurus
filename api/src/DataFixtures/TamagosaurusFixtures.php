be652a0d6d3b41dd57f29d6dce71e25ce5a0c8db
yy0416 <wheathare@gmail.com>
2023-05-25 09:20:51 +0200

    add la prorieté last_feed sous tamagosaurus

--- api/src/DataFixtures/TamagosaurusFixtures.php
+++ api/src/DataFixtures/TamagosaurusFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Species;
 use App\Entity\Tamagosaurus;
 use App\Repository\SpeciesRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -9,19 +10,22 @@ use Doctrine\Persistence\ObjectManager;
 
 class TamagosaurusFixtures extends Fixture
 {
-    public const TYPES = [
-        'T-rex',
-        'Type2',
-        'Type3'
-    ];
+    // public const TYPES = [
+    //     'T-rex',
+    //     'Type2',
+    //     'Type3'
+    // ];
     public function load(ObjectManager $manager): void
     {
-        foreach (self::TYPES as $type) {
-            $saurus = new Tamagosaurus();
-            $saurus->setType($type);
-            $manager->persist($saurus);
-            $this->addReference('Tamagosaurus_' . $type, $saurus);
-        }
+        // foreach (self::TYPES as $type) {
+        $saurus = new Tamagosaurus();
+        $type = new Species();
+        $type->setName('T-Rex');
+        $saurus->setType($type);
+        $saurus->setName('YY');
+        $manager->persist($saurus);
+        // $this->addReference('Tamagosaurus_1', $saurus);
+        // }
         $manager->flush();
     }
 }
--- api/src/Repository/TamagosaurusRepository.php
+++ api/src/Repository/TamagosaurusRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Tamagosaurus;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+
 /**
  * @extends ServiceEntityRepository<Tamagosaurus>
  *
@@ -39,28 +40,28 @@ class TamagosaurusRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Tamagosaurus[] Returns an array of Tamagosaurus objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Tamagosaurus[] Returns an array of Tamagosaurus objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Tamagosaurus
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Tamagosaurus
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
