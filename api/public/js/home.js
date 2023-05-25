e363bac1d45417037ca853e95c022d52bab38a13
LouiseVen <louise.venuleth@laposte.net>
2023-05-25 10:37:46 +0200

    Dino egg

--- api/public/js/home.js
+++ api/public/js/home.js
@@ -1,12 +1,23 @@
 function openRegister() {
-    let form = document.getElementById('testForm');
+    let form = document.getElementById('registerForm');
     
     form.classList.replace('formHidden','formVisible');
 }
 
 function openLogin() {
+    let form = document.getElementById('loginForm');
+    
+    form.classList.replace('formHidden','formVisible');
+}
 
-    div.classList.toggle('formVisible');
+function closeRegister() {
+    let form = document.getElementById('registerForm');
 
-    document.getElementsByTagName('body')[0].appendChild(div);
+    form.classList.replace('formVisible', 'formHidden');
+}
+
+function closeLogin() {
+    let form = document.getElementById('loginForm');
+
+    form.classList.replace('formVisible', 'formHidden');
 }
