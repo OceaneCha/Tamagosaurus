25a7f1eab0d6e921f3610790a64da2079a0710ed
Sarahbokar <bokarthiresarah@gmail.com>
2023-05-25 17:09:06 +0200

    buttons.html.twig

--- api/public/js/api/request.js
+++ api/public/js/api/request.js
@@ -8,11 +8,11 @@ const putRequest = async (resource, json) => {
         body: JSON.stringify(json)
     });
 
-    return await response.json();
-}
+  return await response.json();
+};
 
 const getRequest = async (resource) => {
-    const response = await fetch(`https://localhost${resource}`);
+  const response = await fetch(`https://localhost${resource}`);
 
-    return await response.json();
-}
+  return await response.json();
+};
