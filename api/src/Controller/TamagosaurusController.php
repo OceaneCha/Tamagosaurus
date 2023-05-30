c04bde450f7078a035fbe3c1df9fed2248e9a53c
OceaneCha <oceane.chabrerie.33@gmail.com>
2023-05-30 11:53:00 +0200

    refactored route names in TamagosaurusController

--- api/src/Controller/TamagosaurusController.php
+++ api/src/Controller/TamagosaurusController.php
@@ -10,10 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/app/tamagosaurus')]
+#[Route('/app/tamagosaurus', name: 'app_tamagosaurus_')]
 class TamagosaurusController extends AbstractController
 {
-    #[Route('/', name: 'app_tamagosaurus_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(TamagosaurusRepository $tamagosaurusRepository): Response
     {
         return $this->render('tamagosaurus/index.html.twig', [
@@ -21,7 +21,7 @@ class TamagosaurusController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_tamagosaurus_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, TamagosaurusRepository $tamagosaurusRepository): Response
     {
         $tamagosauru = new Tamagosaurus();
@@ -40,7 +40,7 @@ class TamagosaurusController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_tamagosaurus_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Tamagosaurus $tamagosauru): Response
     {
         return $this->render('tamagosaurus/show.html.twig', [
@@ -48,7 +48,7 @@ class TamagosaurusController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_tamagosaurus_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Tamagosaurus $tamagosauru, TamagosaurusRepository $tamagosaurusRepository): Response
     {
         $form = $this->createForm(TamagosaurusType::class, $tamagosauru);
@@ -66,7 +66,7 @@ class TamagosaurusController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_tamagosaurus_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Tamagosaurus $tamagosauru, TamagosaurusRepository $tamagosaurusRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $tamagosauru->getId(), $request->request->get('_token'))) {
